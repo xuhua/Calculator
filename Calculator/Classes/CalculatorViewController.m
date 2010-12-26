0f16b7895dcef42da456928f5d9b0f61c4df939f
Hua Xu <xuhua_alex@hotmail.com>
2010-12-26 13:22:33 -0500

    Implemented Caculator basic

--- Calculator/Classes/CalculatorViewController.m
+++ Calculator/Classes/CalculatorViewController.m
@@ -10,56 +10,34 @@
 
 @implementation CalculatorViewController
 
-
-
-/*
-// The designated initializer. Override to perform setup that is required before the view is loaded.
-- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil {
-    if ((self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil])) {
-        // Custom initialization
-    }
-    return self;
+- (CalculatorBrain *)brain
+{
+	if (!brain) {
+		brain = [[CalculatorBrain alloc] init];
+		return brain;
+	}
 }
-*/
+- (IBAction)digitPressed:(UIButton *)sender
+{
+	NSString *digit = [[sender titleLabel] text];
+	if (userIsInTheMiddleOfTypingANumber) {
+		[display setText:[[display text] stringByAppendingString:digit]];
+	} else {
+		[display setText:digit];
+		userIsInTheMiddleOfTypingANumber = YES;
+	}
 
-/*
-// Implement loadView to create a view hierarchy programmatically, without using a nib.
-- (void)loadView {
-}
-*/
-
-
-/*
-// Implement viewDidLoad to do additional setup after loading the view, typically from a nib.
-- (void)viewDidLoad {
-    [super viewDidLoad];
-}
-*/
-
-
-/*
-// Override to allow orientations other than the default portrait orientation.
-- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation {
-    // Return YES for supported orientations
-    return (interfaceOrientation == UIInterfaceOrientationPortrait);
-}
-*/
-
-- (void)didReceiveMemoryWarning {
-	// Releases the view if it doesn't have a superview.
-    [super didReceiveMemoryWarning];
-	
-	// Release any cached data, images, etc that aren't in use.
 }
 
-- (void)viewDidUnload {
-	// Release any retained subviews of the main view.
-	// e.g. self.myOutlet = nil;
-}
-
-
-- (void)dealloc {
-    [super dealloc];
+- (IBAction)operationPressed:(UIButton *)sender
+{
+	if (userIsInTheMiddleOfTypingANumber) {
+		[[self brain] setOperand:[[display text] doubleValue]];
+		userIsInTheMiddleOfTypingANumber = NO;
+	}
+	NSString *operation = [[sender titleLabel] text];
+	double result = [[self brain] performOperation:operation];
+	[display setText:[NSString stringWithFormat:@"%g", result]];
 }
 
 @end
